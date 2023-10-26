6f0b09fe1ca0698338afa5ff9406826233d25eed
Steve Austin <stevenrobertaustin@gmail.com>
2023-10-26 12:23:01 -0400

    Remove Compose UI (#4)

--- sample-app/src/main/java/com/bennyapi/MainActivity.kt
+++ sample-app/src/main/java/com/bennyapi/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.bennyapi
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.viewinterop.AndroidView
+import android.widget.LinearLayout
 import com.bennyapi.apply.BennyApplyFlow
 import com.bennyapi.apply.BennyApplyListener
 import com.bennyapi.apply.BennyApplyParameters
@@ -14,10 +11,29 @@ import com.bennyapi.apply.BennyApplyParameters.Credentials
 
 private const val LOG_TAG = "Benny"
 
-class MainActivity : ComponentActivity(), BennyApplyListener {
+class MainActivity : Activity(), BennyApplyListener {
+
+    private lateinit var flow: BennyApplyFlow
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { BennyApplyFlowScreen(listener = this) }
+        setContentView(R.layout.main)
+        flow = BennyApplyFlow(
+            context = applicationContext,
+            listener = this,
+            parameters = BennyApplyParameters(
+                credentials = Credentials(
+                    organizationId = "",
+                    clientSecret = "",
+                ),
+            ),
+        )
+        findViewById<LinearLayout>(R.id.main_layout).addView(flow)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        flow.start(externalId = "")
     }
 
     override fun onExit() {
@@ -27,30 +43,4 @@ class MainActivity : ComponentActivity(), BennyApplyListener {
     override fun onDataExchange(applicantDataId: String) {
         Log.d(LOG_TAG, "On data exchange called: $applicantDataId")
     }
-}
-
-@Composable
-private fun BennyApplyFlowScreen(listener: BennyApplyListener) {
-    Surface {
-        BennyApplyApplyFlow(listener = listener)
-    }
-}
-
-@Composable
-private fun BennyApplyApplyFlow(listener: BennyApplyListener) {
-    AndroidView(
-        factory = {
-            BennyApplyFlow(
-                context = it,
-                listener = listener,
-                parameters = BennyApplyParameters(
-                    credentials = Credentials(
-                        organizationId = "",
-                        clientSecret = "",
-                    ),
-                ),
-            )
-        },
-        update = { it.start(externalId = "") },
-    )
 }
