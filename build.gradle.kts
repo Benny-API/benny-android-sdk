c0c609948bd1dfac97008780bfc1a8b6c7ee1ba9
Steve Austin <steve@bennyapi.com>
2023-11-16 13:20:41 -0500

    Check for and update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.library") version "8.1.3" apply false
     id("com.diffplug.spotless") version "6.22.0" apply true
+    id("com.github.ben-manes.versions") version "0.49.0" apply true
 }
 
 spotless {
