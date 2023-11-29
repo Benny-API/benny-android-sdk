69de11726122d5939c10957f577ee6ba1efc14ef
Steve Austin <steve@bennyapi.com>
2023-11-29 16:19:43 -0500

    Add interface method for copying to clipboard (#18)

--- benny-android/src/main/java/com/bennyapi/apply/webview/BennyApplyJavascriptInterface.kt
+++ benny-android/src/main/java/com/bennyapi/apply/webview/BennyApplyJavascriptInterface.kt
@@ -1,9 +1,18 @@
 package com.bennyapi.apply.webview
 
+import android.content.ClipData.newPlainText
+import android.content.ClipboardManager
 import android.webkit.JavascriptInterface
 import com.bennyapi.apply.BennyApplyListener
 
-internal class BennyApplyJavascriptInterface(private val listener: BennyApplyListener) {
+internal class BennyApplyJavascriptInterface(
+    private val listener: BennyApplyListener,
+    private val clipboardManager: ClipboardManager,
+) {
+    @JavascriptInterface
+    fun copyToClipboard(label: String, text: String) {
+        clipboardManager.setPrimaryClip(newPlainText(label, text))
+    }
 
     @JavascriptInterface
     fun onExit() = listener.onExit()
