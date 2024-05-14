e81871a256b63734c76c730aaa48270cc7efb8e0
Steve Austin <steve@bennyapi.com>
2024-05-14 18:46:49 -0400

    Do not override URL loading (#31)

--- benny-android/src/main/java/com/bennyapi/ebtbalancelink/webview/EbtBalanceLinkWebViewClient.kt
+++ benny-android/src/main/java/com/bennyapi/ebtbalancelink/webview/EbtBalanceLinkWebViewClient.kt
@@ -1,14 +1,11 @@
 package com.bennyapi.ebtbalancelink.webview
 
-import android.content.Intent
-import android.content.Intent.ACTION_VIEW
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 internal class EbtBalanceLinkWebViewClient : WebViewClient() {
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-        view.context.startActivity(Intent(ACTION_VIEW, request.url))
-        return true
+        return false
     }
 }
